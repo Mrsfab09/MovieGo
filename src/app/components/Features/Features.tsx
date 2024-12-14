3e52bd06a4dcf4a6e6e617df72fc2ecbd54552dd
MrsFab09 <ytfabiskora07@gmail.com>
2024-12-14 22:29:34 +0100

    adding new component Features

---
+++ src/app/components/Features/Features.tsx
@@ -0,0 +1,13 @@
+export function Features() {
+  return (
+    <div className="relative isolate px-6 lg:px-8 transparent">
+      <div className="mx-auto max-w-2xl py-28 sm:py-32 lg:py-32">
+        <div className="text-center sm:mb-8 sm:flex sm:justify-center">
+          <h2 className="text-gray-300 font-semibold tracking-tight text-4xl">
+            Innovative Features
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}

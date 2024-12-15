85d2a0672d21db857236dd42ff5864f4d15c1e91
MrsFab09 <ytfabiskora07@gmail.com>
2024-12-15 21:58:11 +0100

    adding new section faq

---
+++ src/app/components/Faq/Faq.tsx
@@ -0,0 +1,30 @@
+export function Faq() {
+  return (
+    <div className="relative isolate px-6 lg:px-8 transparent">
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-28 -z-1 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff433dc9] to-[#ff433d7c] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+        />
+      </div>
+      <div className="mx-auto max-w-7xl py-28 sm:py-32 lg:py-32">
+        <div className="text-center max-w-2xl flex flex-col mx-auto sm:mb-8 ">
+          <h2 className="text-gray-300 font-semibold tracking-tight text-5xl">
+            Innovative Features
+          </h2>
+          <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            soluta assumenda accusantium temporibus beatae praesentium
+            exercitationem esse fugit expedita nemo.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

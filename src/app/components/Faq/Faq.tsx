885031263028e5ee3ddf2f55db069e144afbde7a
MrsFab09 <ytfabiskora07@gmail.com>
2024-12-26 23:11:15 +0100

    adding answer and question to section faq

--- src/app/components/Faq/Faq.tsx
+++ src/app/components/Faq/Faq.tsx
@@ -1,4 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 export function Faq() {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+
+  const faqs = [
+    {
+      question: "What is MovieGo",
+      answer:
+        "Next.js is a React framework for building static and dynamic websites and web applications.",
+    },
+    {
+      question: "How much does access to the application cost?",
+      answer:
+        "Tailwind CSS is a utility-first CSS framework for rapid UI development.",
+    },
+    {
+      question: "Where can I watch?",
+      answer:
+        'You can install Next.js by running the command "npx create-next-app@latest".',
+    },
+    {
+      question: "How to unsubscribe",
+      answer:
+        'Install Tailwind by running "npm install -D tailwindcss postcss autoprefixer".',
+    },
+  ];
   return (
     <div className="relative isolate px-6 lg:px-8 transparent">
       <div
@@ -14,7 +42,7 @@ export function Faq() {
         />
       </div>
       <div className="relative mx-auto max-w-7xl py-28 sm:py-32 lg:py-32">
-        <div className="text-center max-w-2xl flex flex-col mx-auto sm:mb-8 ">
+        <div className="text-center max-w-2xl flex flex-col mx-auto sm:mb-8">
           <h2 className="text-gray-300 font-semibold tracking-tight text-5xl">
             Frequently Asked Questions
           </h2>
@@ -23,6 +51,29 @@ export function Faq() {
             soluta assumenda accusantium temporibus beatae praesentium
             exercitationem esse fugit expedita nemo.
           </p>
+        </div>
+
+        <div className="flex flex-wrap md:flex-nowrap justify-start space-x-8">
+          <div className="w-full md:w-1/3 space-y-4">
+            {faqs.map((faq, index) => (
+              <div
+                key={index}
+                className="p-4 backdrop-blur-md bg-gradient-to-r from-white/10 to-white/5 rounded-lg"
+                onMouseEnter={() => setHoveredIndex(index)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <h3 className="text-lg font-semibold">{faq.question}</h3>
+              </div>
+            ))}
+          </div>
+
+          <div className="w-full md:w-2/3 pl-6 mt-6 md:mt-0">
+            {hoveredIndex !== null && (
+              <div className="p-6 backdrop-blur-md bg-gradient-to-r from-white/10 to-white/5 rounded-lg">
+                <p>{faqs[hoveredIndex].answer}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>

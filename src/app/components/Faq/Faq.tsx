75a2b9679f47f3dba498973a1e685836480077a0
MrsFab09 <ytfabiskora07@gmail.com>
2025-01-12 20:26:09 +0100

    change classes in tailwind

--- src/app/components/Faq/Faq.tsx
+++ src/app/components/Faq/Faq.tsx
@@ -7,22 +7,22 @@ export function Faq() {
 
   const faqs = [
     {
-      question: "What is MovieGo",
+      question: "01 What is MovieGo",
       answer:
         "Next.js is a React framework for building static and dynamic websites and web applications.",
     },
     {
-      question: "How much does access to the application cost?",
+      question: "02 How much does access to the application cost?",
       answer:
         "Tailwind CSS is a utility-first CSS framework for rapid UI development.",
     },
     {
-      question: "Where can I watch?",
+      question: "03 Where can I watch?",
       answer:
         'You can install Next.js by running the command "npx create-next-app@latest".',
     },
     {
-      question: "How to unsubscribe",
+      question: "04 How to unsubscribe",
       answer:
         'Install Tailwind by running "npm install -D tailwindcss postcss autoprefixer".',
     },
@@ -41,7 +41,7 @@ export function Faq() {
           className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff433dc9] to-[#ff433d7c] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
         />
       </div>
-      <div className="relative mx-auto max-w-7xl py-28 sm:py-32 lg:py-32">
+      <div className="relative mx-auto max-w-7xl py-28 sm:py-32 lg:py-32 mb-48">
         <div className="text-center max-w-2xl flex flex-col mx-auto sm:mb-8">
           <h2 className="text-gray-300 font-semibold tracking-tight text-5xl">
             Frequently Asked Questions
@@ -53,23 +53,25 @@ export function Faq() {
           </p>
         </div>
 
-        <div className="flex flex-wrap md:flex-nowrap justify-start space-x-8">
-          <div className="w-full md:w-1/3 space-y-4">
+        <div className="flex flex-wrap md:flex-nowrap justify-center space-x-8 px-28">
+          <div className="w-full md:w-2/3 space-y-4 align-center">
             {faqs.map((faq, index) => (
               <div
                 key={index}
-                className="p-4 backdrop-blur-md bg-gradient-to-r from-white/10 to-white/5 rounded-lg"
+                className="p-4 "
                 onMouseEnter={() => setHoveredIndex(index)}
                 onMouseLeave={() => setHoveredIndex(null)}
               >
-                <h3 className="text-lg font-semibold">{faq.question}</h3>
+                <h3 className="text-slate-300 text-3xl font-semibold mb-4">
+                  {faq.question}
+                </h3>
               </div>
             ))}
           </div>
 
           <div className="w-full md:w-2/3 pl-6 mt-6 md:mt-0">
             {hoveredIndex !== null && (
-              <div className="p-6 backdrop-blur-md bg-gradient-to-r from-white/10 to-white/5 rounded-lg">
+              <div className="w-80 h-96 text-2xl flex-shrink-0 p-6 rotate-3 rounded-lg backdrop-blur-md bg-gradient-to-r from-white/5 to-white/3">
                 <p>{faqs[hoveredIndex].answer}</p>
               </div>
             )}

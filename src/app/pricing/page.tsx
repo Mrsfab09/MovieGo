ad6fb9dc14ebf9ab053157aed531d4d97f10a4cd
MrsFab09 <ytfabiskora07@gmail.com>
2025-01-14 22:27:21 +0100

    adding carousel component to pricing route

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,3 +1,10 @@
+import { Carousel } from "../components/Carousel/Carousel";
+
 export default function Pricing() {
-  return <div>Pricing</div>;
+  return (
+    <>
+      <div>Pricing</div>
+      <Carousel />
+    </>
+  );
 }

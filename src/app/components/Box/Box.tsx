94d90dde24932ddba9e58253103d23b342c3977b
MrsFab09 <ytfabiskora07@gmail.com>
2025-01-14 14:46:58 +0100

    adding responsive boxes

--- src/app/components/Box/Box.tsx
+++ src/app/components/Box/Box.tsx
@@ -32,7 +32,7 @@ export function Box() {
     },
   ];
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-6 p-6">
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-6 p-6">
       {descriptions.map((item, index) => (
         <div
           key={index}

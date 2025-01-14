f63f1e7ee23b789daf5ccf1dc20f52826eee0109
MrsFab09 <ytfabiskora07@gmail.com>
2025-01-14 22:26:08 +0100

    adding login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,29 @@
-export default function Login() {
+"use client";
+
+import { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  const { isSignedIn } = useUser(); // Hook do sprawdzenia, czy użytkownik jest zalogowany
+
+  useEffect(() => {
+    if (isSignedIn) {
+      // Jeśli użytkownik jest zalogowany, przekieruj go na stronę dashboard
+      window.location.href = "/dashboard"; // lub użyj routera Next.js
+    }
+  }, [isSignedIn]);
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <SignIn routing="hash" />
     </div>
   );
 }

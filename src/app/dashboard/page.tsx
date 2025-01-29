c9f48a35dd6715874fd33688aaf62df88d73cb15
MrsFab09 <ytfabiskora07@gmail.com>
2025-01-29 23:12:19 +0100

    adding logo in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import { LogoutButton } from "../components/LogoutButton/LogoutButton";
 
 export default function Dashboard() {
   return (
     <>
+      <Image width={200} height={200} src={"/assets/logoGo.png"} alt="" />
       <div>Welcome in Dashboard</div>
       <LogoutButton />
     </>

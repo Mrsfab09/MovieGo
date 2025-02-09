9da1e330eea01e99f01271c326652223523b6f81
MrsFab09 <ytfabiskora07@gmail.com>
2025-02-09 21:40:02 +0100

    refactor: move faq data to a separate file

---
+++ src/app/data/faqData.ts
@@ -0,0 +1,22 @@
+export const faqs = [
+  {
+    question: "What is MovieGo?",
+    answer:
+      "Next.js is a React framework for building static and dynamic websites and web applications.",
+  },
+  {
+    question: "How much does access to the application cost?",
+    answer:
+      "Tailwind CSS is a utility-first CSS framework for rapid UI development.",
+  },
+  {
+    question: "Where can I watch?",
+    answer:
+      'You can install Next.js by running the command "npx create-next-app@latest".',
+  },
+  {
+    question: "How to unsubscribe?",
+    answer:
+      'Install Tailwind by running "npm install -D tailwindcss postcss autoprefixer".',
+  },
+];

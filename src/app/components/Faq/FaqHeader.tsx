ffa12068cf1b834f6945b324ab3593d7e9873c4a
MrsFab09 <ytfabiskora07@gmail.com>
2025-02-09 21:40:17 +0100

    feat: create FaqHeader component

---
+++ src/app/components/Faq/FaqHeader.tsx
@@ -0,0 +1,14 @@
+export function FaqHeader() {
+  return (
+    <div className="text-center max-w-2xl flex flex-col mx-auto sm:mb-8">
+      <h2 className="text-gray-300 font-semibold tracking-tight text-5xl">
+        Frequently Asked Questions
+      </h2>
+      <p className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        soluta assumenda accusantium temporibus beatae praesentium
+        exercitationem esse fugit expedita nemo.
+      </p>
+    </div>
+  );
+}

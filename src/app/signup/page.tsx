69759d091a5a20fd6955c8da1bc44601cab07cc9
MrsFab09 <ytfabiskora07@gmail.com>
2025-02-18 19:45:26 +0100

    style: changed style form and optimalization image

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,5 +1,4 @@
 import { SignUp } from "@clerk/nextjs";
-// import { withTheme } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 import { Footer } from "../components/Footer/Footer";
@@ -8,13 +7,19 @@ export default function SignUpPage() {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen">
-        {/* <SignUp routing="hash" /> */}
         <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1200px] min-h-[750px] md:h-[700px] bg-neutral-900 rounded-lg shadow-xl border border-gray-700 overflow-hidden p-6">
           {/* Lewa część – Obrazek */}
-          <div
-            className="hidden md:block bg-cover bg-center opacity-70"
-            style={{ backgroundImage: "url('/assets/popcorn-cinema.jpg')" }}
-          ></div>
+          <div className="relative hidden md:block w-full h-full opacity-80">
+            <Image
+              src="/assets/popcorn-cinema.jpg"
+              alt="Popcorn Cinema"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              quality={75} 
+              loading="lazy" 
+            />
+          </div>
 
           {/* Prawa część – Formularz */}
           <div className="flex flex-col justify-center items-center w-full">
@@ -31,12 +36,12 @@ export default function SignUpPage() {
               appearance={{
                 elements: {
                   rootBox: "p-3",
-                  card: "bg-gray-800 text-white p-6 rounded-lg w-full max-w-md",
+                  card: "bg-neutral-900 text-white p-6 rounded-lg w-full max-w-md",
                   headerTitle: "text-2xl font-bold text-gray-100",
                   socialButtonsBlockButton: "bg-gray-700 hover:bg-gray-600",
                   formFieldInput: "bg-gray-700 border-none text-white",
                   formButtonPrimary:
-                    "bg-red-900 hover:bg-red-600 text-white p-2",
+                    "bg-red-900 hover:bg-red-600 text-white p-2 ",
                 },
               }}
             />

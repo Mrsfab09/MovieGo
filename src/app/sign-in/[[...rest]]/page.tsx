e91a3808c12b4d63b3c2148f155870b73d48fbe7
MrsFab09 <ytfabiskora07@gmail.com>
2025-02-23 17:17:41 +0100

    delete comments

--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -2,28 +2,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return (
-    <SignIn
-      afterSignInUrl="/dashboard" // Określenie, gdzie użytkownik ma zostać przekierowany po zalogowaniu
-    />
-  );
+  return <SignIn afterSignInUrl="/dashboard" />;
 }
-
-// "use client";
-
-// import { SignIn } from "@clerk/nextjs";
-// import { useRouter } from "next/navigation";
-
-// export default function SignInPage() {
-//   const router = useRouter();
-
-//   const handleSignIn = () => {
-//     router.push("/dashboard");
-//   };
-
-//   return (
-//     <div>
-//       <SignIn routing="hash" afterSignInUrl="/dashboard" />
-//     </div>
-//   );
-// }

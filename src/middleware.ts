02e5ec24b9d29f121a2730158a4202dd88170931
hades <twojemail@example.com>
2025-03-05 09:17:45 +0100

    delete comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|static|favicon.ico|sign-in|sign-up).*)", // Upewnij się, że middleware nie blokuje sign-in i sign-up
-    "/(api|trpc)(.*)", // Middleware dla API
+    "/((?!_next|static|favicon.ico|sign-in|sign-up).*)", 
+    "/(api|trpc)(.*)", 
   ],
 };

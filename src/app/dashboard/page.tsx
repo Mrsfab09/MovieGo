2d6eb63a16d35c07e7a93de7e9a9c2c96a3fd4d0
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-09 23:17:41 +0100

    fix: returning web

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,40 +1,24 @@
-import { auth } from '@clerk/nextjs/server';
-import { Page404 } from "../components/Page404/Page404";
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
 import LogoutButton from "../components/LogoutButton/LogoutButton";
+import { Page404 } from "../components/Page404/Page404";
 
-// Typowanie dla user
-type User = {
-  id: string;
-  email: string;
-};
-
-// Komponent serverowy (renderowany po stronie serwera)
 export default async function DashboardPage() {
   try {
-    // Uzyskanie danych użytkownika za pomocą Clerk
-    const { userId, user } = await auth();
+    const { userId } = await auth();
 
-    // Jeśli użytkownik nie jest zalogowany
-    if (!userId || !user) {
+    if (!userId) {
       return <Page404 />;
     }
 
-    // Zbieramy dane użytkownika
-    const userData: User = {
-      id: userId,
-      email: user.email || "",
-    };
-
-    // Zwracamy dashboard z danymi użytkownika
     return (
       <>
-        <p>Witaj, {userData.id}!</p>
-        <p>Email: {userData.email}</p>
         <LogoutButton />
       </>
     );
   } catch (error) {
-    console.error('Błąd podczas logowania:', error);
-    return <Page404 />;
+    console.error("Błąd podczas logowania", error);
+    return <p>Wystąpił błąd podczas logowania.</p>;
   }
 }

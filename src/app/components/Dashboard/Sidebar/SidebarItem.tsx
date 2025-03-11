211654444d219b9f4b13e632bc0df1b213b18d81
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-11 21:53:14 +0100

    create new component SidebarItem

---
+++ src/app/components/Dashboard/Sidebar/SidebarItem.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { SidebarItemProps } from "../../../types/movieTypes";
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon,
+  label,
+  href,
+  active = false,
+}) => (
+  <Link href={href} className="block">
+    <div
+      className={`flex flex-col items-center justify-center p-3 rounded-lg transition-colors duration-200 ${
+        active
+          ? "bg-red-700 text-white"
+          : "text-gray-400 hover:bg-neutral-800 hover:text-white"
+      }`}
+      title={label}
+    >
+      <div className="text-center">{icon}</div>
+    </div>
+  </Link>
+);
+
+export default SidebarItem;

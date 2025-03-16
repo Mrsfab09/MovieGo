9c37498c779efbf85e5f02917e4660a6cabc58e9
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-16 19:09:32 +0100

    adding style to div

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -6,9 +6,11 @@ import Sidebar from "../components/Dashboard/Sidebar/Sidebar";
 export default function Settings() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
-    <div className="flex">
+    <>
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      Settings
-    </div>
+      <div className="flex-1 md:ml-16 bg-neutral-950 min-h-screen">
+        Settings
+      </div>
+    </>
   );
 }

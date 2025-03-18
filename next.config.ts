5d4a64ac08e98d19fd4f52eac0358a6c56373c78
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-18 22:47:44 +0100

    changed domains and remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,20 @@ module.exports = {
     clerk: true,
   },
   images: {
-    domains: ["image.tmdb.org"],
+    domains: ["image.tmdb.org", "media.themoviedb.org"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "image.tmdb.org", // Ensure both tmdb image domains are included
+      },
+      {
+        protocol: "https",
+        hostname: "media.themoviedb.org", // Ensure this is added as well
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com", // This domain is for Unsplash if you're using it elsewhere
       },
     ],
   },
 };
-// "dev": "NODE_OPTIONS='--inspect' next dev",

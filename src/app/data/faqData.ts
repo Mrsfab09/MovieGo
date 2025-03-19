1aa5a38b139243aba21989c2fe4119b4b426c8ed
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-19 22:21:43 +0100

    update data faq answers

--- src/app/data/faqData.ts
+++ src/app/data/faqData.ts
@@ -2,21 +2,21 @@ export const faqs = [
   {
     question: "What is MovieGo?",
     answer:
-      "lorem Ips incorrectly, but I'm not sure what the correct answer is.",
+      "MovieGo offers personalized recommendations, trending films, and a seamless watching experience, featuring everything from classics to blockbusters.",
   },
   {
     question: "How much does access to the application cost?",
     answer:
-      "lorem Ips incorrectly, but I'm not sure what the correct answer is.",
+      "MovieGo is free to use! Some premium features may require a subscription, but you can enjoy many movies and recommendations without any cost.",
   },
   {
     question: "Where can I watch?",
     answer:
-      "lorem Ips incorrectly, but I'm not sure what the correct answer is.",
+      "Watch movies on MovieGo anytime, anywhere on your computer, tablet, or smartphone with an internet connection.",
   },
   {
     question: "How to unsubscribe?",
     answer:
-      "lorem Ips incorrectly, but I'm not sure what the correct answer is.",
+      "To unsubscribe, go to your account settings and click 'Unsubscribe.' You’ll no longer be charged, and your access will end at the end of the billing cycle.",
   },
 ];

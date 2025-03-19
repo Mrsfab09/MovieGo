a3a4496673a85815f8b62b0c189d502681714dd4
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-19 21:15:11 +0100

    adding simple settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,15 +1,45 @@
 "use client";
 
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { Sidebar } from "../components/Dashboard/Sidebar/Sidebar";
+import { UserProfile, useUser } from "@clerk/clerk-react";
+import Image from "next/image";
+
+interface SettingsProps {
+  emailAddresses: ReactNode;
+}
 
 export default function Settings() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { user } = useUser();
+
   return (
     <>
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      <div className="flex-1 md:ml-16 bg-neutral-950 min-h-screen">
-        Settings
+      <div className="flex-1 md:ml-16">
+        <div className="flex items-center justify-center mt-5 sm:mt-32 lg:mt-20">
+          <UserProfile />
+        </div>
+        {/* <h2 className="text-3xl font-medium ml-32 mt-12 mb-10">Settings</h2> */}
+        {/* <div className="flex items-center justify-between ml-32">
+          <div>
+            <p>Profile</p>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center mx-auto mt-10 overflow-hidden gap-5">
+          <Image
+            width={"40"}
+            height={"40"}
+            src={
+              user?.imageUrl && user.imageUrl !== ""
+                ? user.imageUrl
+                : "/default-avatar.png"
+            }
+            alt="Profile Picture"
+            className="w-20 h-20 rounded-full"
+          />
+          <h3 className="text-xl">Test</h3>
+        </div> */}
       </div>
     </>
   );

8c9d460a4b75a4b01737a9b19bb4c2acfbaf6104
MrsFab09 <ytfabiskora07@gmail.com>
2025-03-19 21:11:02 +0100

    add new hostname clerk

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ module.exports = {
     clerk: true,
   },
   images: {
-    domains: ["image.tmdb.org", "media.themoviedb.org"],
+    domains: ["image.tmdb.org", "media.themoviedb.org", "img.clerk.com"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com", // This domain is for Unsplash if you're using it elsewhere
+      },
       {
         protocol: "https",
         hostname: "image.tmdb.org", // Ensure both tmdb image domains are included
